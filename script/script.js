0709c397bfb45b3d53e6bb9ba2b4b5da7bd52413
Singh-Nirbhayjot <15320992@itisrossi.vi.it>
2025-12-19 19:38:01 +0100

    "Aggiunte immagini e modificati file js e file css"

--- script/script.js
+++ script/script.js
@@ -21,59 +21,90 @@ function toglitutto(){
     document.getElementById('giocom').innerHTML="";
     document.getElementById('giocod').innerHTML="";
 }
+
 function facile() {
-    pricarta = null;   //pricarta significa primacarta
-    seccarta = null;   //seccarta seconda carta non avevo voglia di scrivere
+    pricarta = null;
+    seccarta = null;
     let coppie = 0;
 
     let giocofacile = document.getElementById('giocof');
     giocofacile.innerHTML = "";
 
-    let mem=[1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];  //mem significa memory
+    let mem = [
+        [1,1,2,2],
+        [3,3,4,4],
+        [5,5,6,6],
+        [7,7,8,8]
+    ];
 
-    for (let i = 0; i < mem.length; i++) {
-        let inrand = Math.floor(Math.random() * mem.length);  //inrand non è un insulto ma significa indicerandom (:
-        let mesc = mem[i];
-        mem[i] = mem[inrand];
-        mem[inrand] = mesc;
+    for (let r = 0; r < mem.length; r++) {
+        for (let c = 0; c < mem[r].length; c++) {
+            let inrandr = Math.floor(Math.random() * mem.length);
+            let inrandc = Math.floor(Math.random() * mem[inrandr].length);
+            let mesc = mem[r][c];
+            mem[r][c] = mem[inrandr][inrandc];
+            mem[inrandr][inrandc] = mesc;
+        }
     }
 
-    for (let i = 0; i < mem.length; i++) {
-        let carta = document.createElement('button');
-        carta.value = mem[i];
-        carta.textContent = "";
+    for (let r = 0; r < mem.length; r++) {
+        for (let c = 0; c < mem[r].length; c++) {
+            let carta = document.createElement('button');
+            carta.value = mem[r][c];
+            carta.textContent = "";
 
-        carta.onclick = function controllo() {
-            if (carta == pricarta || carta.disabled==true){ 
-                return;
-            }
-            carta.textContent = carta.value;
-            if (pricarta == null) {
-                pricarta = carta;
-            } 
-            else if(seccarta==null)
+            let foto = document.createElement('img');
+            foto.src = "img/0.jpg"; 
+            carta.appendChild(foto);
+            carta.foto = foto;
+
+            carta.onclick = function controllo() {
+                if (carta == pricarta || carta.disabled==true){
+                    return;
+                }
+                if (pricarta != null && seccarta != null){
+                    return;
+                }
+
+                carta.foto.src = "img/" + carta.value + ".jpg";
+
+                if (pricarta == null) {
+                    pricarta = carta;
+                }
+                else if(seccarta==null)
                 {
-                seccarta = carta;
-                if (pricarta.value == seccarta.value) {
+                    seccarta = carta;
+
+                    if (pricarta.value == seccarta.value) {
                         pricarta.disabled = true;
                         seccarta.disabled = true;
                         coppie++;
-                } else {
-                        pricarta.textContent = "";
-                        seccarta.textContent = "";
-                }
-                    pricarta = null;
-                    seccarta = null;
 
-                    if (coppie == 8) {
-                        alert("BRAVO HAI VINTO");
+                        pricarta = null;
+                        seccarta = null;
+
+                        if (coppie == 8) {
+                            alert("BRAVO HAI VINTO");
+                        }
+                    } else {
+                        let a = pricarta;
+                        let b = seccarta;
+
+                        setTimeout(function(){
+                            a.foto.src = "img/0";
+                            b.foto.src = "img/0";
+                            pricarta = null;
+                            seccarta = null;
+                        }, 700);
                     }
+                }
+            }
+
+            giocofacile.appendChild(carta);
         }
     }
-        giocof.appendChild(carta);
-    
 }
-}
+
 function media() {
     pricarta = null;
     seccarta = null;
@@ -82,50 +113,80 @@ function media() {
     let giocom = document.getElementById('giocom');
     giocom.innerHTML = "";
 
-    let mem=[1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12];
+    let mem = [
+        [1,1,2,2,3,3],
+        [4,4,5,5,6,6],
+        [7,7,8,8,9,9],
+        [10,10,11,11,12,12]
+    ];
 
-    for (let i = 0; i < mem.length; i++) {
-        let inrand = Math.floor(Math.random() * mem.length);
-        let mesc = mem[i];
-        mem[i] = mem[inrand];
-        mem[inrand] = mesc;
+    for (let r = 0; r < mem.length; r++) {
+        for (let c = 0; c < mem[r].length; c++) {
+            let inrandr = Math.floor(Math.random() * mem.length);
+            let inrandc = Math.floor(Math.random() * mem[inrandr].length);
+            let mesc = mem[r][c];
+            mem[r][c] = mem[inrandr][inrandc];
+            mem[inrandr][inrandc] = mesc;
+        }
     }
 
-    for (let i = 0; i < mem.length; i++) {
-        let carta = document.createElement('button');
-        carta.value = mem[i];
-        carta.textContent = "";
+    for (let r = 0; r < mem.length; r++) {
+        for (let c = 0; c < mem[r].length; c++) {
+            let carta = document.createElement('button');
+            carta.value = mem[r][c];
+            carta.textContent = "";
 
-        carta.onclick = function controllo() {
-            if (carta == pricarta || carta.disabled==true){ 
-                return;
-            }
-            carta.textContent = carta.value;
-            if (pricarta == null) {
-                pricarta = carta;
-            } 
-            else if(seccarta==null)
-            {
-                seccarta = carta;
-                if (pricarta.value == seccarta.value) {
+            let foto = document.createElement('img');
+            foto.src = "img/0.jpg";
+            carta.appendChild(foto);
+
+            carta.foto = foto;
+
+            carta.onclick = function controllo() {
+                if (carta == pricarta || carta.disabled==true){
+                    return;
+                }
+                if (pricarta != null && seccarta != null){
+                    return;
+                }
+
+                carta.foto.src = "img/" + carta.value + ".jpg";
+
+                if (pricarta == null) {
+                    pricarta = carta;
+                }
+                else if(seccarta==null)
+                {
+                    seccarta = carta;
+
+                    if (pricarta.value == seccarta.value) {
                         pricarta.disabled = true;
                         seccarta.disabled = true;
                         coppie++;
-                } else {
-                        pricarta.textContent = "";
-                        seccarta.textContent = "";
-                }
-                    pricarta = null;
-                    seccarta = null;
 
-                    if (coppie == 12) {
-                        alert("BRAVO HAI VINTO");
+                        pricarta = null;
+                        seccarta = null;
+
+                        if (coppie == 12) {
+                            alert("BRAVO HAI VINTO");
+                        }
+                    } else {
+                        let a = pricarta;
+                        let b = seccarta;
+
+                        setTimeout(function(){
+                            a.foto.src = "0";
+                            b.foto.src = "0";
+                            pricarta = null;
+                            seccarta = null;
+                        }, 700);
                     }
+                }
+            }
+
+            giocom.appendChild(carta);
         }
     }
-        giocom.appendChild(carta);
-    
-}
 }
 
 function difficile() {
@@ -136,47 +197,78 @@ function difficile() {
     let giocod = document.getElementById('giocod');
     giocod.innerHTML = "";
 
-    let mem=[1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15,16,16];
+    let mem = [
+        [1,1,2,2,3,3,4,4],
+        [5,5,6,6,7,7,8,8],
+        [9,9,10,10,11,11,12,12],
+        [13,13,14,14,15,15,16,16]
+    ];
 
-    for (let i = 0; i < mem.length; i++) {
-        let inrand = Math.floor(Math.random() * mem.length);
-        let mesc = mem[i];
-        mem[i] = mem[inrand];
-        mem[inrand] = mesc;
+    for (let r = 0; r < mem.length; r++) {
+        for (let c = 0; c < mem[r].length; c++) {
+            let inrandr = Math.floor(Math.random() * mem.length);
+            let inrandc = Math.floor(Math.random() * mem[inrandr].length);
+            let mesc = mem[r][c];
+            mem[r][c] = mem[inrandr][inrandc];
+            mem[inrandr][inrandc] = mesc;
+        }
     }
 
-    for (let i = 0; i < mem.length; i++) {
-        let carta = document.createElement('button');
-        carta.value = mem[i];
-        carta.textContent = "";
+    for (let r = 0; r < mem.length; r++) {
+        for (let c = 0; c < mem[r].length; c++) {
+            let carta = document.createElement('button');
+            carta.value = mem[r][c];
+            carta.textContent = "";
 
-        carta.onclick = function controllo() {
-            if (carta == pricarta || carta.disabled==true){ 
-                return;
-            }
-            carta.textContent = carta.value;
-            if (pricarta == null) {
-                pricarta = carta;
-            } 
-            else if(seccarta==null)
-            {
-                seccarta = carta;
-                if (pricarta.value == seccarta.value) {
+            let foto = document.createElement('img');
+            foto.src = "img/0.jpg";
+            carta.appendChild(foto);
+
+            carta.foto = foto;
+
+            carta.onclick = function controllo() {
+                if (carta == pricarta || carta.disabled==true){
+                    return;
+                }
+                if (pricarta != null && seccarta != null){
+                    return;
+                }
+
+                carta.foto.src = "img/" + carta.value + ".jpg";
+
+                if (pricarta == null) {
+                    pricarta = carta;
+                }
+                else if(seccarta==null)
+                {
+                    seccarta = carta;
+
+                    if (pricarta.value == seccarta.value) {
                         pricarta.disabled = true;
                         seccarta.disabled = true;
                         coppie++;
-                } else {
-                        pricarta.textContent = "";
-                        seccarta.textContent = "";
-                }
-                    pricarta = null;
-                    seccarta = null;
 
-                    if (coppie == 16) {
-                        alert("BRAVO HAI VINTO");
+                        pricarta = null;
+                        seccarta = null;
+
+                        if (coppie == 16) {
+                            alert("BRAVO HAI VINTO");
+                        }
+                    } else {
+                        let a = pricarta;
+                        let b = seccarta;
+
+                        setTimeout(function(){
+                            a.foto.src = "0";
+                            b.foto.src = "0";
+                            pricarta = null;
+                            seccarta = null;
+                        }, 700);
                     }
+                }
+            }
+
+            giocod.appendChild(carta);
         }
     }
-        giocod.appendChild(carta);
-}
 }
